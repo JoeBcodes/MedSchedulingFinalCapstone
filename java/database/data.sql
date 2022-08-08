565a8743e9f0f3e17b0be618476ce724d8993cc3
Joe Baca <jbaca324@hotmail.com>
2022-08-08 17:11:15 -0500

    dropped patient and doctor tables

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,4 +3,9 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO doctors (doctor_id, first_name, last_name, phone, email)
+
+
 COMMIT TRANSACTION;
+
+
