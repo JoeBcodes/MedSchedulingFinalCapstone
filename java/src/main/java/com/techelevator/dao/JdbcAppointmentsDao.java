198d28104fbc8654f120ede9f21eac382e0eadd0
Joy Wang <joywang93@gmail.com>
2022-08-09 15:35:03 -0600

    updated appt classes, methods, and attributes

--- java/src/main/java/com/techelevator/dao/JdbcAppointmentsDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcAppointmentsDao.java
@@ -1,5 +1,6 @@
 package com.techelevator.dao;
 
+import com.techelevator.model.Appointments;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +13,36 @@ public class JdbcAppointmentsDao implements AppointmentsDao{
         this.jdbcTemplate = jdbcTemplate;
     }
 
+//    @Override
+//    public int findIdByApptDate() {}
+
+
+    //change to userId
+
+//isAvailable should be false, should only be visible to Drs// must also display the patient name
+    //sql use OR
     @Override
-    public int findIdBy
+    public Appointments findAllBookedApptsByDoctor(int userId, boolean isAvaialble) {
+        return null;
+    }
+
+
+    //find all by specific dr, should only be visible to patients
+    @Override
+    public Appointments findAllAvailableApptsByDoctor(int doctorId, boolean isAvailable) {
+        return null;
+    }
+
+
+    //find all regardless of Dr, only viewable by patient
+    @Override
+    public Appointments findAllAvailableAppts(boolean isAvailable) {
+        return null;
+    }
+
+//    @Override
+//    public Appointments findAllApptByPatient(int patientId) {
+//        return null;
+//    }
 
 }
--- java/src/main/java/com/techelevator/model/Appointments.java
+++ java/src/main/java/com/techelevator/model/Appointments.java
@@ -1,4 +1,112 @@
 package com.techelevator.model;
 
 public class Appointments {
+
+    private int apptId;
+    //change to userId
+    private int doctorId;
+    private int patientId;
+    private String apptDate;
+    private String apptTime;
+    private String purposeOfVisit;
+    private boolean isRead;
+    private boolean isAvailable;
+    private String doctorName;
+    private String patientName;
+
+    public Appointments() {
+    }
+
+    ;
+
+    public Appointments(int apptId, int doctorId, int patientId, String apptDate, String apptTime, String purposeOfVisit,
+                        boolean isCompleted, boolean isAvailable) {
+        this.apptId = apptId;
+        this.doctorId = doctorId;
+        this.patientId = patientId;
+        this.apptDate = apptDate;
+        this.apptTime = apptTime;
+        this.purposeOfVisit = purposeOfVisit;
+        this.isRead = isRead;
+        this.isAvailable = isAvailable;
+    }
+
+
+    public int getApptId() {
+        return apptId;
+    }
+
+    public void setApptId(int apptId) {
+        this.apptId = apptId;
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(int doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getApptDate() {
+        return apptDate;
+    }
+
+    public void setApptDate(String apptDate) {
+        this.apptDate = apptDate;
+    }
+
+    public String getApptTime() {
+        return apptTime;
+    }
+
+    public void setApptTime(String apptTime) {
+        this.apptTime = apptTime;
+    }
+
+    public String getPurposeOfVisit() {
+        return purposeOfVisit;
+    }
+
+    public void setPurposeOfVisit(String purposeOfVisit) {
+        this.purposeOfVisit = purposeOfVisit;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public void setRead(boolean completed) {
+        isRead = completed;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Appointment{" +
+                "apptId=" + apptId +
+                ", doctorId='" + doctorId +
+                ", patientId=" + patientId +
+                ", apptDate=" + apptDate +
+                ", apptTime=" + apptTime +
+                ", purposeOfVisit=" + purposeOfVisit +
+                ", isRead=" + isRead +
+                ", isAvailable=" + isAvailable +
+                '}';
+    }
 }
